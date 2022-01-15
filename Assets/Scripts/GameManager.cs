269fe07661577e75ae3b806a41d37973c9054187
ceron999 <ceron999@naver.com>
2022-01-16 04:30:19 +0900

    220116/정윤석/gameManager 주석 추가
    꼭 함수 설명을 주석으로 달아주세용!!

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    /*
+    함수에 관한 내용은 주석으로 꼭!!!!!!!!!!!!!!!!!!!!!!! 써주세요!!!!
+     */
     public static GameManager gameManager;
     void Awake()
     {
