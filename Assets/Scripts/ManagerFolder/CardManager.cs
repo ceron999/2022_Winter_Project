4ecbaab34f422a80d75fc085ecc37a49a2c3fd2a
ceron999 <ceron999@naver.com>
2022-02-04 21:22:45 +0900

    220204/정윤석//SelectCard 매니저들 추가
    gameManager
    cardManager
    characterManager 임의적 추가
    나중에 연결되게 할 것.

---
+++ Assets/Scripts/ManagerFolder/CardManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardManager : MonoBehaviour
+{
+    public static CardManager cardManager;
+
+
+    //플레이어 큐
+    //적의 큐
+    //큐 2개 합친 큐
+
+    //etc...
+    void Start()
+    {
+        cardManager = this;
+    }
+
+}
