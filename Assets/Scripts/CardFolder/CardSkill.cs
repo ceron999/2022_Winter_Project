8b5e763d47dfbd46dd9caafae1e2db2853b240e0
ceron999 <ceron999@naver.com>
2022-02-21 22:23:42 +0900

    220221/정윤석/ 카드 스킬 스크립트 추가
    해당 스크립트는 스킬 카드의 범위 등을 저장해 gameManager에서 활용하도록 할 생각입니다.

---
+++ Assets/Scripts/CardFolder/CardSkill.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardSkill : MonoBehaviour
+{
+    /// 캐릭터 포지션 기준 타일 위치.
+    /// (-4, 2)  (-2, 2)  ( 0, 2)  ( 2, 2)  ( 4, 2)
+    /// (-4, 1)  (-2, 1)  ( 0, 1)  ( 2, 1)  ( 4, 1)
+    /// (-4, 0)  (-2, 0)  ( 0, 0)  ( 2, 0)  ( 4, 0)
+    /// (-4,-1)  (-2,-1)  ( 0,-1)  ( 2,-1)  ( 4,-1)
+    /// (-4,-2)  (-2,-2)  ( 0,-2)  ( 2,-2)  ( 4,-2)
+    
+    ///각 포지션 인덱스 (12가 캐릭터 기준)
+    /// 0  1  2  3  4
+    /// 5  6  7  8  9
+    ///10 11 12 13 14
+    ///15 16 17 18 19
+    ///20 21 22 23 24
+
+    public List<Vector2> rangeVecter;
+
+    private void Start()
+    {
+        
+    }
+
+    void SetRangeVecter()
+    {
+        
+    }
+
+    void SetRangeIdx()
+    {
+        //gilbert Range
+        
+        //walwha Range
+
+        //patrick Range
+    }
+
+    public void Skill()
+    {
+
+    }
+
+    public void Move()
+    {
+        
+    }
+}
